b57435e21280145574dd3ef55edc6f0786bb53a9
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-26 16:25:39 -0500

    complete mvp for app

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,13 +31,25 @@ const data = [
 class Dashboard extends Component {
   state = {
     loading: false,
+    focused: null,
   };
+
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
+
   render() {
-    const dashboardClasses = classnames('dashboard');
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused,
+    });
 
     if (this.state.loading) return <Loading />;
 
-    const panels = data.map((item) => <Panel key={item.id} id={item.id} label={item.label} value={item.value} />);
+    const panels = (this.state.focused ? data.filter((panel) => this.state.focused === panel.id) : data).map((panel) => (
+      <Panel key={panel.id} label={panel.label} value={panel.value} onSelect={() => this.selectPanel(panel.id)} />
+    ));
 
     return <main className={dashboardClasses}> {panels}</main>;
   }
